7230aed43770f7b36e01647538c8d594a6abbb81
SelvinToledo <selvin.toledo193@tectijuana.edu.mx>
2022-04-30 13:26:42 -0700

    proyectoexpo

---
+++ programas.s
@@ -0,0 +1,39 @@
+	.arch armv6
+	.eabi_attribute 28, 1
+	.eabi_attribute 20, 1
+	.eabi_attribute 21, 1
+	.eabi_attribute 23, 3
+	.eabi_attribute 24, 1
+	.eabi_attribute 25, 1
+	.eabi_attribute 26, 2
+	.eabi_attribute 30, 4
+	.eabi_attribute 34, 1
+	.eabi_attribute 18, 4
+	.file	"programac.c"
+	.text
+	.section	.text.startup,"ax",%progbits
+	.align	2
+	.global	main
+	.arch armv6
+	.syntax unified
+	.arm
+	.fpu vfp
+	.type	main, %function
+main:
+	@ args = 0, pretend = 0, frame = 0
+	@ frame_needed = 0, uses_anonymous_args = 0
+	push	{r4, lr}
+	ldr	r0, .L3
+	bl	printf
+	mov	r0, #0
+	pop	{r4, pc}
+.L4:
+	.align	2
+.L3:
+	.word	.LC0
+	.size	main, .-main
+	.section	.rodata.str1.1,"aMS",%progbits,1
+.LC0:
+	.ascii	"Hola mundo\000"
+	.ident	"GCC: (Raspbian 8.3.0-6+rpi1) 8.3.0"
+	.section	.note.GNU-stack,"",%progbits
